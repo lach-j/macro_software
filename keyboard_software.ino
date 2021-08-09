cb44b516175adc01e691c4254359bf0d11cbdcf1
Lachlan Johnson <lachlanm.johnson@gmail.com>
2021-08-09 18:45:38 +1000

    Keys mapped to  functions

--- keyboard_software.ino
+++ keyboard_software.ino
@@ -5,27 +5,45 @@
 const byte ROWS = 3;
 const byte COLS = 5;
 const byte LAYERS = 2;
+
+int curr_lyr = 0;
+int rgb_pins[] = {3, 6, 5};
+
+int layer_colors[LAYERS][3] = {{255, 0, 0}, {0, 255, 0}};
+
 char keys[ROWS][COLS] = {
-    {'a','b','c','d','e'},
-    {'f','g','h','i','j'},
-    {'k','l','m','n','o'},
+    {'A','B','C','D','E'},
+    {'F','G','H','I','J'},
+    {'K','L','M','N','O'},
 };
 
 void EMPTY(int state) {
+  Serial.println("empty");
+}
+
+void N_LYR(int state) {
+  if (state == PRESSED) {
+    if (curr_lyr >= LAYERS-1) {
+      curr_lyr = 0;
+    } else {
+      curr_lyr++;
+    }
+    setRGB(layer_colors[curr_lyr]);
+    Serial.println(curr_lyr);
+  }
 }
 
 
-
 void (*Macros[LAYERS][ROWS*COLS])(int state) = {
-  {
+  { // WINDOWS
     EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,
     EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,
-    EMPTY, EMPTY, EMPTY, EMPTY, EMPTY
+    EMPTY, EMPTY, EMPTY, EMPTY, N_LYR
   },
-  {
+  { // LINUX
     EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,
     EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,
-    EMPTY, EMPTY, EMPTY, EMPTY, EMPTY
+    EMPTY, EMPTY, EMPTY, EMPTY, N_LYR
   }
 };
 
@@ -46,5 +64,11 @@ void loop(){
 }
 
 void keypadEvent(KeypadEvent key){
-  Serial.println(key);
+  Macros[0][key-65](keypad.getState());
+}
+
+void setRGB(int rgb[3]) {
+  for (int i=0; i<3; i++) {
+    analogWrite(rgb_pins[i], rgb[i]);
+  }
 }
